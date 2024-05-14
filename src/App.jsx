686e5de72e9ec985164fc0178cdea180f4cfa6de
MISHKAUA <marishka.pichik@gmail.com>
2024-05-14 10:20:36 +0200

    Add random colour component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Accordion from "./components/accordion";
+import { RandomColour } from "./components/random_colour";
 
 function App() {
   return (
     <div className="App">
       <Accordion />
+      <hr className="m-4" />
+      <RandomColour />
     </div>
   );
 }
--- src/components/random_colour/index.jsx
+++ src/components/random_colour/index.jsx
@@ -1,1 +1,34 @@
-export function RandomColour() {}
+import { useState } from "react";
+
+export function RandomColour() {
+  const [colour, setColour] = useState("#0000FF");
+
+  function randomColourUtility(length) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function createColour() {
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexColour = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hexColour += hex[randomColourUtility(hex.length)];
+    }
+    console.log(hexColour);
+    setColour(hexColour);
+  }
+  return (
+    <div
+      className="container flex flex-col items-center justify-center height:100vh width:100wh"
+      style={{ background: colour }}
+    >
+      <button
+        onClick={() => createColour()}
+        className="border-2 rounded-md px-4 py-4 mx-4"
+      >
+        Generate random colour
+      </button>
+      <div className="text-blue text-4xl my-10">{colour}</div>
+    </div>
+  );
+}
